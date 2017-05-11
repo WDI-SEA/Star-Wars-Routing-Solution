12fcc72da5bd72fe65e58b9228860070f85e95bc
darkenvy <darkenvy6@gmail.com>
2017-05-11 11:22:04 -0700

    solution

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('StarWarsApp', ['StarWarsCtrls', 'ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/404');
+
+  // define routes
+  $stateProvider
+    .state('films', {
+      url: '/',
+      templateUrl: 'views/films.html',
+      controller: 'HomeCtrl'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'views/about.html'
+    })
+    .state('showFilm', {
+      url: '/films/:id',
+      templateUrl: 'views/filmShow.html',
+      controller: 'FilmShowCtrl'
+    })
+}])
